8b8a33e1d5afe1dd185ac21af777bd88d2eba8cd
brian.mcnamara <brian.mcnamara@salesforce.com>
2024-09-23 12:45:44 -0700

    Update deprecated methods and warnings

--- tunneler/src/main/kotlin/com/salesforce/pomerium/PomeriumAuthProvider.kt
+++ tunneler/src/main/kotlin/com/salesforce/pomerium/PomeriumAuthProvider.kt
@@ -1,36 +1,26 @@
 package com.salesforce.pomerium
 
-import com.jetbrains.rd.framework.util.startAsync
-import com.jetbrains.rd.framework.util.synchronizeWith
 import com.jetbrains.rd.util.lifetime.Lifetime
-import io.ktor.client.HttpClient
+import com.jetbrains.rd.util.threading.coroutines.async
+import com.jetbrains.rd.util.threading.coroutines.synchronizeWith
+import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import io.ktor.server.application.call
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.route
-import io.ktor.server.routing.routing
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.runBlocking
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
 import org.apache.http.client.utils.URIBuilder
 import org.slf4j.LoggerFactory
 import java.io.Closeable
 import java.net.URI
 import java.util.concurrent.ConcurrentHashMap
-import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLSocketFactory
-import javax.net.ssl.TrustManager
 import javax.net.ssl.X509TrustManager
 
 /**
@@ -87,7 +77,7 @@ class PomeriumAuthProvider (
             val callbackServer = PomeriumAuthCallbackServer()
             val serverPort = callbackServer.start()
 
-            LOG.info("Starting HTTP server on port ${serverPort} for pomerium auth token callback")
+            LOG.info("Starting HTTP server on port $serverPort for pomerium auth token callback")
 
             val authLink = getAuthLink(route, pomeriumPort, serverPort)
             val credString = getCredString(authLink)
@@ -107,12 +97,12 @@ class PomeriumAuthProvider (
                 }
                 routeToCredKeyMap[route] = credString
                 val isNewRoute = existingRoutes.add(route)
-                val getToken = jobLifetime.startAsync(Dispatchers.Default) {
+                val getToken = jobLifetime.async(Dispatchers.Default) {
                     try {
                         val auth = callbackServer.getToken()
                         LOG.info("Successfully acquired Pomerium authentication")
                         credentialStore.setToken(credString, auth)
-                        return@startAsync auth
+                        return@async auth
                     } finally {
                         withContext(NonCancellable) {
                             credKeyToMutexMap[credString]!!.withLock {
@@ -145,7 +135,7 @@ class PomeriumAuthProvider (
             //Port does not matter in this case
             val link = getAuthLink(route, pomeriumPort, 8080)
             getCredString(link)
-        }).let {
+        }).also {
             credKeyToMutexMap.computeIfAbsent(it) { Mutex() }.withLock {
                 credentialStore.clearToken(it)
                 credKeyToAuthJobMap.remove(it)
