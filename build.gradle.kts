b834635a73371d0d9bbe8d26a054d911ffb8da24
brian.mcnamara <brian.mcnamara@salesforce.com>
2024-09-23 11:36:39 -0700

    Upgrade to Gateway 2024.2, JDK21 and kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 intellijPlatform {
